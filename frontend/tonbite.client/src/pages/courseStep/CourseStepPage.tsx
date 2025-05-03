1c0ad5a8a23aa9ae690964963ac2d56d86b5bbdf
zena <91028915+0zena@users.noreply.github.com>
2025-05-03 14:26:51 +0200

    Export Course Step Content to PDF (#24)
    * export: export course step content to PDF
    
    * export: code clean up
    
    * export: overflow bug fix

--- frontend/tonbite.client/src/pages/courseStep/CourseStepPage.tsx
+++ frontend/tonbite.client/src/pages/courseStep/CourseStepPage.tsx
@@ -2,16 +2,20 @@ import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import {CourseStepService} from "../../services";
 import {CourseStepType} from "../../states";
+import {Icons} from "../../utils/Icons.ts";
+import {usePDF} from "react-to-pdf";
 
 // UI Components
 import {CourseStepContent} from "./CourseStepContent.tsx";
 import {NotFoundError} from "../error/NotFoundError.tsx";
-import {LoadingLayout} from "../../components";
+import {Icon, LoadingLayout} from "../../components";
+import {Button} from "@heroui/react";
 
 export const CourseStepPage = () => {
     const [step, setStep] = useState<CourseStepType | undefined>();
     const [loading, setLoading] = useState(false);
     const {id} = useParams();
+    const {toPDF, targetRef} = usePDF({filename: `course-step-${id}.pdf`});
 
     useEffect(() => {
         setLoading(true);
@@ -21,15 +25,22 @@ export const CourseStepPage = () => {
         });
     }, [id]);
 
+    const handleDownload = () => {
+        toPDF();
+    }
+
 
     if (loading) return <LoadingLayout />;
     if (!loading && !step) return <NotFoundError />
 
     return (
-      <div className={"space-y-6"}>
-          <h3 className={"text-4xl font-bold text-blue-400"}>{step?.name}</h3>
-          <p className={"text-xl text-start border-b pb-2"}>{step?.bio}</p>
-          {step?.content && (<CourseStepContent html={step.content} />)}
-      </div>
+        <div className={"course-step space-y-6 p-4 h-full overflow-y-hidden"}>
+            <h3 className={"text-4xl font-bold text-blue-400"}>{step?.name}</h3>
+            <p className={"text-xl text-start border-b pb-2"}>{step?.bio}</p>
+            {step?.content && (<Button startContent={<Icon icon={Icons.PDF} />} onPress={handleDownload}>Download PDF</Button>)}
+            <div ref={targetRef} className="max-h-full h-full">
+                {step?.content && (<CourseStepContent html={step.content} />)}
+            </div>
+        </div>
     );
 }
